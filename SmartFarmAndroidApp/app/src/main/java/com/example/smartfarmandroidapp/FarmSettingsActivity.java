27ffa96976c5a7518c7e307f251299d6255c26ab
fanimator12 <torok.franciska.leonora@gmail.com>
2021-04-25 16:06:32 +0200

    added value validation in settings view

--- SmartFarmAndroidApp/app/src/main/java/com/example/smartfarmandroidapp/FarmSettingsActivity.java
+++ SmartFarmAndroidApp/app/src/main/java/com/example/smartfarmandroidapp/FarmSettingsActivity.java
@@ -2,24 +2,30 @@ package com.example.smartfarmandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.smartfarmandroidapp.viewmodel.CO2ViewModel;
 import com.example.smartfarmandroidapp.viewmodel.HumidityViewModel;
 import com.example.smartfarmandroidapp.viewmodel.TemperatureViewModel;
 
 public class FarmSettingsActivity extends AppCompatActivity {
-    private EditText desiredCO2, CO2Min, CO2Max;
-    private EditText desiredTemperature, temperatureMin, temperatureMax;
-    private EditText desiredHumidity, humidityMin, humidityMax;
-    private Button saveButton, returnButton;
+    EditText desiredCO2, CO2Min, CO2Max;
+    EditText desiredTemperature, temperatureMin, temperatureMax;
+    EditText desiredHumidity, humidityMin, humidityMax;
+    Button saveButton, returnButton;
+    TextView info;
 
-    private CO2ViewModel CO2ViewModel;
-    private TemperatureViewModel temperatureViewModel;
-    private HumidityViewModel humidityViewModel;
+    CO2ViewModel CO2ViewModel;
+    TemperatureViewModel temperatureViewModel;
+    HumidityViewModel humidityViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,15 +45,43 @@ public class FarmSettingsActivity extends AppCompatActivity {
         saveButton = findViewById(R.id.saveThresholdButton);
         returnButton = findViewById(R.id.returnFromTweakingButton);
 
-        saveButton.setOnClickListener(v -> {
-            CO2ViewModel.updateThresholds(getNumber(desiredCO2), getNumber(CO2Min), getNumber(CO2Max));
-            temperatureViewModel.updateThresholds(getNumber(desiredTemperature), getNumber(temperatureMin), getNumber(temperatureMax));
-            humidityViewModel.updateThresholds(getNumber(desiredHumidity), getNumber(humidityMin), getNumber(humidityMax));
-        });
+        info = findViewById(R.id.errorTextView);
+
+        saveButton.setOnClickListener(view -> onClick(info));
 
         returnButton.setOnClickListener(v -> { finish(); });
     }
 
+    @SuppressLint("SetTextI18n")
+    public void onClick(View view){
+        validateValues(Float.parseFloat(desiredCO2.getText().toString()),
+                Float.parseFloat(CO2Min.getText().toString()),
+                Float.parseFloat(CO2Max.getText().toString()),
+                Float.parseFloat(desiredHumidity.getText().toString()),
+                Float.parseFloat(humidityMin.getText().toString()),
+                Float.parseFloat(humidityMax.getText().toString()),
+                Float.parseFloat(desiredTemperature.getText().toString()),
+                Float.parseFloat(temperatureMin.getText().toString()),
+                Float.parseFloat(temperatureMax.getText().toString()));
+
+        CO2ViewModel.updateThresholds(getNumber(desiredCO2), getNumber(CO2Min), getNumber(CO2Max));
+        temperatureViewModel.updateThresholds(getNumber(desiredTemperature), getNumber(temperatureMin), getNumber(temperatureMax));
+        humidityViewModel.updateThresholds(getNumber(desiredHumidity), getNumber(humidityMin), getNumber(humidityMax));
+
+        Context context = getApplicationContext();
+        String text = "Update values...";
+        int duration = Toast.LENGTH_SHORT;
+        Toast.makeText(context, text, duration).show();
+    }
+
+    private void validateValues(float desiredCO2, float CO2Min, float CO2Max, float desiredHum, float humMin, float humMax, float desiredTemp, float minTemp, float maxTemp){
+        if((desiredTemp > 10 && desiredTemp < 50) || (desiredHum > 5 && desiredHum < 95) || (desiredCO2 > 5 && desiredCO2 < 95)){
+            Intent intent = new Intent(FarmSettingsActivity.this, MonitorActivity.class);
+            startActivity(intent);
+        } else {
+            info.setText("Something went wrong");
+        }
+    }
 
     private float getNumber(TextView textView) { return Float.parseFloat(textView.getText().toString()); }
 }
