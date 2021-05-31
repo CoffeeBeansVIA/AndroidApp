96ac931ccc5299799e223fd5936331e4925a61ab
Igork777 <igorlopatchin27@gmail.com>
2021-05-31 12:55:25 +0200

    Forgot to commit in the last commit

--- SmartFarmAndroidApp/app/src/main/java/com/example/smartfarmandroidapp/Domain/Measurement.java
+++ SmartFarmAndroidApp/app/src/main/java/com/example/smartfarmandroidapp/Domain/Measurement.java
@@ -1,18 +1,25 @@
 package com.example.smartfarmandroidapp.Domain;
 
+public class Measurement {
+    private int id;
 
-public class CO2 {
+    public int getId() {
+        return id;
+    }
 
     private double value;
-
     public double getValue() {
         return value;
     }
-
     private String time;
     public String getTime()
     {
         return time;
     }
+    private MeasurementSensor sensor;
 
+    public MeasurementSensor getMeasurementSensor() {
+        return sensor;
+    }
 }
+
